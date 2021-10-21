dde82a6a2da447386c1b134dd0aa707166106f27
Admin <asanurb1205@g.educaand.es>
2021-10-21 12:21:11 +0200

    El lector es funcional

--- src/main/java/sinGoogle/Lector.java
+++ src/main/java/sinGoogle/Lector.java
@@ -1,18 +1,25 @@
 package sinGoogle;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Lector
 {
-    public static void main(String[] args) throws Exception
-    {
-//parsing a CSV file into Scanner class constructor
-        Scanner sc = new Scanner(new File("/media/alejandro/ALEX/Proyectos Java/Elector/src/main/java/sinGoogle/listado.csv"));
-        sc.useDelimiter(",");   //sets the delimiter pattern
-        while (sc.hasNext())  //returns a boolean value
-        {
-            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+    public static void main(String[] args) {
+        String line;
+        String splitBy = "\n";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/media/alejandro/ALEX/Proyectos Java/Elector/src/main/java/sinGoogle/listado.csv"));
+            while ((line = br.readLine()) != null)
+            {
+                String[] employee = line.split(splitBy);
+                System.out.println(Arrays.toString(employee));
+            }
         }
-        sc.close();  //closes the scanner
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
