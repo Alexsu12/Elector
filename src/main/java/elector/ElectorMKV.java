1718d1773d450ad59a03c19fc884f1f0fc7451d6
Alejandro <asanurb1205@g.educaand.es>
2022-02-01 14:40:41 +0100

    Fix

--- src/main/java/elector/ElectorMKV.java
+++ src/main/java/elector/ElectorMKV.java
@@ -3,46 +3,62 @@ package elector;
 import java.sql.*;
 
 public class ElectorMKV {
-    public static void main(String[] args)
-    {
-        try
-        {
-            // create our mysql database connection
-            String myDriver = "org.gjt.mm.mysql.Driver";
-            String myUrl = "jdbc:mysql://192.168.42.128:3306";
-            Class.forName(myDriver);
-            Connection conn = DriverManager.getConnection(myUrl, "user", "user");
+    public static void main(String[] args) {
 
-            // our SQL SELECT query.
-            // if you only need a few columns, specify them by name instead of using "*"
-            String query = "SELECT * FROM users";
+        Connection connection = null;
+        try {
+            // Load the MySQL JDBC driver
+            String driverName = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driverName);
 
-            // create the java statement
-            Statement st = conn.createStatement();
 
-            // execute the query, and get a java resultset
-            ResultSet rs = st.executeQuery(query);
+            // Create a connection to the database
+            String serverName = "192.168.42.128";
+            String schema = "Alumnos";
+            String url = "jdbc:mysql://" + serverName + "/" + schema;
+            String username = "user";
+            String password = "user";
 
-            // iterate through the java resultset
-            while (rs.next())
-            {
-                int id = rs.getInt("id");
-                String firstName = rs.getString("first_name");
-                String lastName = rs.getString("last_name");
-                Date dateCreated = rs.getDate("date_created");
-                boolean isAdmin = rs.getBoolean("is_admin");
-                int numPoints = rs.getInt("num_points");
+            connection = DriverManager.getConnection(url, username, password);
 
-                // print the results
-                System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);
-            }
-            st.close();
+
+            System.out.println("Successfully Connected to the database!\n");
+
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Could not find the database driver " + e.getMessage());
+        } catch (SQLException e) {
+
+            System.out.println("Could not connect to the database " + e.getMessage());
         }
-        catch (Exception e)
-        {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
+
+        try {
+            // Get a result set containing all data from test_table
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM Listado");
+
+
+            // For each row of the result set ...
+            while (results.next()) {
+                String data = results.getString("idAlumno");
+                System.out.println("Id" + ": " + data + " ");
+                // Get the data from the current row using the column index - column data are in the VARCHAR format
+                data = results.getString("nombre");
+                System.out.println("Nombre" + ": " + data + " ");
+                // Get the data from the current row using the column name - column data are in the VARCHAR format
+                data = results.getString("apellido1");
+                System.out.println("Apellido 1" + ": " + data + " ");
+
+                data = results.getString("apellido2");
+                if (data.equals("")){
+                    System.out.println("\n");
+                }else System.out.println("Apellido 2" + ": " + data + "\n");
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not retrieve data from the database " + e.getMessage());
         }
     }
 }
+
 
